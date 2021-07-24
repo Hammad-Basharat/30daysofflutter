3202a2f780d58b5776d66c2f4bf6066d0578c8c6
Hammad-Basharat <hammadaulakh@gmail.com>
2021-07-24 21:57:18 +0500

    day2 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,13 +5,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
-    final String name = "Faisalabad";
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text("Catalog App"))),
+      appBar: AppBar(
+        title: Center(child: Text("Catalog App")),
+      ),
       body: Center(
         child: Container(
-          child: Text("Welcome to $days days of flutter in $name"),
+          child: Text("Welcome to 30 days of flutter."),
         ),
       ),
       drawer: Drawer(),
