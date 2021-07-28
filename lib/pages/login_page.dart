d63f175de399f359bca6a5c4b14df2cd9a8db116
Hammad-Basharat <hammadaulakh@gmail.com>
2021-07-28 22:31:11 +0500

    day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -23,33 +23,35 @@ class LoginPage extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Enter User Name",
-                  labelText: "User Name",
+          Padding(padding:  const EdgeInsets.symmetric(vertical: 16, horizontal: 16 ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter User Name",
+                    labelText: "User Name",
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Enter Password",
-                  labelText: "Password",
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print("AOA");
-                  },
-                  child: Text("login"))
-            ],
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print("AOA");
+                    },
+                    child: Text("login"))
+              ],
+            ),
           ),
         ],
       ),
